fc6a8f87e59788d58bea97f81c52add112cf851e
Julie Qiu <julieyeqiu@gmail.com>
2020-05-10 12:27:09 -0400

    cmd/fetch: fix build error

--- cmd/fetch/main.go
+++ cmd/fetch/main.go
@@ -33,7 +33,7 @@ func newShopper() (*internal.Shopper, error) {
 		return internal.NewShopperWithCookies([]*http.Cookie{{Name: baldorCookieName, Value: baldorCookieValue}})
 	}
 
-	email, pass, err := readEmailAndPassword()
+	email, pass, err := internal.ReadEmailAndPassword()
 	if err != nil {
 		return nil, err
 	}
@@ -42,45 +42,7 @@ func newShopper() (*internal.Shopper, error) {
 	if err != nil {
 		return nil, err
 	}
-	printCookies(shopper.Jar)
 	return shopper, nil
-}
-
-func readEmailAndPassword() (string, string, error) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Enter email: ")
-	email, err := reader.ReadString('\n')
-	if err != nil {
-		return "", "", err
-	}
-	fmt.Printf("Enter password: ")
-	pass, err := reader.ReadString('\n')
-	if err != nil {
-		return "", "", err
-	}
-	return strings.TrimSuffix(email, "\n"), strings.TrimSuffix(pass, "\n"), nil
-}
-
-func printCookies(j http.CookieJar) error {
-	var found bool
-	cookies, err := internal.BaldorCookies(j)
-	if err != nil {
-		return err
-	}
-	for _, c := range cookies {
-		if c.Name == "provided_access" || c.Name == "PHPSESSID" {
-			continue
-		}
-		found = true
-		log.Println()
-		log.Printf("export BALDORFOOD_COOKIE_NAME=%q\n", c.Name)
-		log.Printf("export BALDORFOOD_COOKIE_VALUE=%q\n", c.Value)
-	}
-	if !found {
-		return fmt.Errorf("Authentication failed.")
-	}
-	log.Println("Save these variables in your .bashrc to skip authenticating in the future.")
-	return nil
 }
 
 func findItems(cookies []*http.Cookie) {
@@ -115,9 +77,9 @@ func findItemsAtURL(u string, cookies []*http.Cookie) error {
 }
 
 func writeItemsToFile(u string, items []*internal.Item) error {
-	filename := "data/" + strings.ReplaceAll(
+	filename := internal.ProductsDir() + "/" + strings.ReplaceAll(
 		strings.TrimSuffix(strings.TrimPrefix(u, "https://www.baldorfood.com/products"), "?viewall=1"),
-		"/", "_") + ".data"
+		"/", "_") + ".json"
 
 	jsonString, err := json.Marshal(items)
 	if err != nil {
